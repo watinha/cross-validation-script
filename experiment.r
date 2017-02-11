abbf89fda2469765db18bcb48575b2f59064ece0
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2017-02-11 00:01:10 -0200

    adding a few other machine learning techniques for the experiment

--- experiment.r
+++ experiment.r
@@ -1,4 +1,6 @@
 library(C50)
+library(e1071)
+library(randomForest)
 library(RWeka)
 library(caret)
 source('lib/cross-validation.r')
@@ -37,11 +39,26 @@ decision_tree <- function (X, y, X_cv, y_cv) {
     cv_predictions <- predict.C5.0(model, X_cv)
     return (table(y_cv, cv_predictions))
 }
+random_forest <- function (X, y, X_cv, y_cv) {
+    model <- randomForest(X, y)
+    cv_predictions <- predict(model, X_cv)
+    return (table(y_cv, cv_predictions))
+}
+s_v_m <- function (X, y, X_cv, y_cv) {
+    model <- svm(X, y)
+    cv_predictions <- predict(model, X_cv)
+    return (table(y_cv, cv_predictions))
+}
 
 folds <- createFolds(dataset_cv[,'Result'])
 crossValidation(dataset_cv, dataset_test, folds, target_features, decision_tree, 'output/decision_tree_target.csv');
 crossValidation(dataset_cv, dataset_test, folds, control_features, decision_tree, 'output/decision_tree_literature.csv');
 
+crossValidation(dataset_cv, dataset_test, folds, target_features, random_forest, 'output/random_forest_target.csv');
+crossValidation(dataset_cv, dataset_test, folds, control_features, random_forest, 'output/random_forest_literature.csv');
+
+crossValidation(dataset_cv, dataset_test, folds, target_features, s_v_m, 'output/svm_target.csv');
+crossValidation(dataset_cv, dataset_test, folds, control_features, s_v_m, 'output/svm_literature.csv');
 
 
 
