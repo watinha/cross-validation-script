711032f29374e5a18516dbc6971a83a296ecf3f0
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2017-02-12 11:45:56 -0200

    now regularizing data for SVM

--- experiment.r
+++ experiment.r
@@ -59,8 +59,32 @@ print('Random forest data collecting ...')
 crossValidation(dataset_cv, dataset_test, folds, target_features, random_forest, 'output/random_forest_target.csv');
 crossValidation(dataset_cv, dataset_test, folds, control_features, random_forest, 'output/random_forest_literature.csv');
 
-print('E1071 - SVM...')
+print('E1071 - SVM... Regularizing nominal features')
+
+dataset_cv[, 'displayed'] <- as.numeric(dataset_cv[,'displayed']) - 1
+
+dataset_test[, 'displayed'] <- as.numeric(dataset_test[,'displayed']) - 1
+
 crossValidation(dataset_cv, dataset_test, folds, target_features, s_v_m, 'output/svm_target.csv');
+
+dataset_cv[, 'table'] <- as.numeric(dataset_cv[,'table']) - 1
+dataset_cv[, 'list'] <- as.numeric(dataset_cv[,'list']) - 1
+dataset_cv[, 'input'] <- as.numeric(dataset_cv[,'input']) - 1
+dataset_cv[, 'widgetName'] <- as.numeric(dataset_cv[,'widgetName']) - 1
+dataset_cv[, 'date'] <- as.numeric(dataset_cv[,'date']) - 1
+dataset_cv[, 'img'] <- as.numeric(dataset_cv[,'img']) - 1
+dataset_cv[, 'proportionNumbers'] <- as.numeric(dataset_cv[,'proportionNumbers']) - 1
+dataset_cv[, 'links80percent'] <- as.numeric(dataset_cv[,'links80percent']) - 1
+
+dataset_test[, 'table'] <- as.numeric(dataset_test[,'table']) - 1
+dataset_test[, 'list']  <- as.numeric(dataset_test[,'list']) - 1
+dataset_test[, 'input'] <- as.numeric(dataset_test[,'input']) - 1
+dataset_test[, 'widgetName'] <- as.numeric(dataset_test[,'widgetName']) - 1
+dataset_test[, 'date'] <- as.numeric(dataset_test[,'date']) - 1
+dataset_test[, 'img']  <- as.numeric(dataset_test[,'img']) - 1
+dataset_test[, 'proportionNumbers'] <- as.numeric(dataset_test[,'proportionNumbers']) - 1
+dataset_test[, 'links80percent']    <- as.numeric(dataset_test[,'links80percent']) - 1
+
 crossValidation(dataset_cv, dataset_test, folds, control_features, s_v_m, 'output/svm_literature.csv');
 
 
