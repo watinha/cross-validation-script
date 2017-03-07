5921c766ae0743c1bb60f445c309ccc9bf104ea9
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2017-03-07 17:41:09 -0300

    re-starting experiment for XBI context

--- experiment.r
+++ experiment.r
@@ -6,33 +6,43 @@ library(caret)
 source('lib/cross-validation.r')
 
 dataset_cv <- read.arff('data/real-dataset.trainning.arff')
-dataset_test <- read.arff('data/real-dataset.test.arff')
 
-target_features <- c('displayed',
-                     'height',
+features1 <- c('height',
                      'width',
-                     'activatorTop',
-                     'activatorLeft',
-                     'distance',
-                     'numberElements',
-                     'elements_per_size',
-                     'numberWords',
-                     'textNodes',
-                     'words_per_textNodes',
-                     'Result')
-
-control_features <- c('distanceTop',
-                      'distanceLeft',
-                      'textNodes',
-                      'table',
-                      'list',
-                      'input',
-                      'widgetName',
-                      'date',
-                      'img',
-                      'proportionNumbers',
-                      'links80percent',
-                      'Result')
+                     'top',
+                     'left',
+                     'parent-top',
+                     'parent-left',
+                     'prev-sibling-top',
+                     'prev-sibling-left',
+                     'next-sibling-top',
+                     'next-sibling-left',
+                     'height1',
+                     'width1',
+                     'top1',
+                     'left1',
+                     'parent-top1',
+                     'parent-left1',
+                     'prev-sibling-top1',
+                     'prev-sibling-left1',
+                     'next-sibling-top1',
+                     'next-sibling-left1',
+                     'height2',
+                     'width2',
+                     'top2',
+                     'left2',
+                     'parent-top2',
+                     'parent-left2',
+                     'prev-sibling-top2',
+                     'prev-sibling-left2',
+                     'next-sibling-top2',
+                     'next-sibling-left2',
+                     'chi-squared',
+                     'chi-squared-1',
+                     'chi-squared-2',
+                     'diff',
+                     'diff-1',
+                     'diff-2')
 
 decision_tree <- function (X, y, X_cv, y_cv) {
     model <- C5.0(X, y)
@@ -52,40 +62,16 @@ s_v_m <- function (X, y, X_cv, y_cv) {
 
 folds <- createFolds(dataset_cv[,'Result'])
 print('C5.0 data collecting - decision tree...')
-crossValidation(dataset_cv, dataset_test, folds, target_features, decision_tree, 'output/decision_tree_target.csv');
-crossValidation(dataset_cv, dataset_test, folds, control_features, decision_tree, 'output/decision_tree_literature.csv');
+crossValidation(dataset_cv, dataset_test, folds, features1, decision_tree, 'output/decision_tree_target.csv');
+#crossValidation(dataset_cv, dataset_test, folds, control_features, decision_tree, 'output/decision_tree_literature.csv');
 
 print('Random forest data collecting ...')
-crossValidation(dataset_cv, dataset_test, folds, target_features, random_forest, 'output/random_forest_target.csv');
-crossValidation(dataset_cv, dataset_test, folds, control_features, random_forest, 'output/random_forest_literature.csv');
+crossValidation(dataset_cv, dataset_test, folds, features1, random_forest, 'output/random_forest_target.csv');
+#crossValidation(dataset_cv, dataset_test, folds, control_features, random_forest, 'output/random_forest_literature.csv');
 
 print('E1071 - SVM... Regularizing nominal features')
-
-dataset_cv[, 'displayed'] <- as.numeric(dataset_cv[,'displayed']) - 1
-
-dataset_test[, 'displayed'] <- as.numeric(dataset_test[,'displayed']) - 1
-
-crossValidation(dataset_cv, dataset_test, folds, target_features, s_v_m, 'output/svm_target.csv');
-
-dataset_cv[, 'table'] <- as.numeric(dataset_cv[,'table']) - 1
-dataset_cv[, 'list'] <- as.numeric(dataset_cv[,'list']) - 1
-dataset_cv[, 'input'] <- as.numeric(dataset_cv[,'input']) - 1
-dataset_cv[, 'widgetName'] <- as.numeric(dataset_cv[,'widgetName']) - 1
-dataset_cv[, 'date'] <- as.numeric(dataset_cv[,'date']) - 1
-dataset_cv[, 'img'] <- as.numeric(dataset_cv[,'img']) - 1
-dataset_cv[, 'proportionNumbers'] <- as.numeric(dataset_cv[,'proportionNumbers']) - 1
-dataset_cv[, 'links80percent'] <- as.numeric(dataset_cv[,'links80percent']) - 1
-
-dataset_test[, 'table'] <- as.numeric(dataset_test[,'table']) - 1
-dataset_test[, 'list']  <- as.numeric(dataset_test[,'list']) - 1
-dataset_test[, 'input'] <- as.numeric(dataset_test[,'input']) - 1
-dataset_test[, 'widgetName'] <- as.numeric(dataset_test[,'widgetName']) - 1
-dataset_test[, 'date'] <- as.numeric(dataset_test[,'date']) - 1
-dataset_test[, 'img']  <- as.numeric(dataset_test[,'img']) - 1
-dataset_test[, 'proportionNumbers'] <- as.numeric(dataset_test[,'proportionNumbers']) - 1
-dataset_test[, 'links80percent']    <- as.numeric(dataset_test[,'links80percent']) - 1
-
-crossValidation(dataset_cv, dataset_test, folds, control_features, s_v_m, 'output/svm_literature.csv');
+crossValidation(dataset_cv, dataset_test, folds, features1, s_v_m, 'output/svm_target.csv');
+#crossValidation(dataset_cv, dataset_test, folds, control_features, s_v_m, 'output/svm_literature.csv');
 
 
 
