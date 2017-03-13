f5e882b7157dd9670d1a00b2bce08feeadbbce36
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2017-03-13 19:26:41 -0300

    learning curve fox

--- lib/learning-curve.r
+++ lib/learning-curve.r
@@ -1,6 +1,6 @@
 source('lib/cross-validation.r')
-learningCurve <- function (dataset, features, model_call) {
-    folds <- createFolds(dataset[,'Result'], k=100)
+learningCurve <- function (dataset_learning, dataset_cv, features, model_call) {
+    folds <- createFolds(dataset_learning[,'Result'], k=100)
     index <- 0
     error_trainning <- c()
     error_cv <- c()
@@ -9,19 +9,18 @@ learningCurve <- function (dataset, features, model_call) {
     for (i in folds) {
         index <- index + 1
         if (is.null(sub_dataset)) {
-            sub_dataset <- dataset[i,]
+            sub_dataset <- dataset_learning[i,]
         } else {
-            sub_dataset <- rbind(sub_dataset, dataset[i,])
+            sub_dataset <- rbind(sub_dataset, dataset_learning[i,])
         }
         sub_folds <- createFolds(sub_dataset[,'Result'], k=10)
         file_name <- paste('output/', index, '.csv', sep='')
-        r <- crossValidation(sub_dataset, sub_dataset, sub_folds, features, model_call, file_name)
-        error_trainning[index] <- (as.double(r[2, 4]) + as.double(r[2, 5])) / nrow(sub_dataset)
-        error_cv[index] <- ((as.double(r[13, 4]) + as.double(r[13, 5])) * 10) / nrow(sub_dataset)
+        r <- crossValidation(sub_dataset, dataset_cv, sub_folds, features, model_call, file_name)
+        error_trainning[index] <- (as.double(r[2, 4]) + as.double(r[2, 5]))
+        error_cv[index] <- ((as.double(r[14, 4]) + as.double(r[14, 5])) * 10)
     }
-    print(error_trainning)
-    print(error_cv)
+    result <- data.frame(error_trainning, error_cv)
     plot(1:100, error_cv, type='o', col='blue', xlim=c(1, 100), ylim=c(0, max(error_trainning, error_cv)))
     lines(1:100, error_trainning, type='o', col='red')
-    return (c)
+    return (result)
 }
